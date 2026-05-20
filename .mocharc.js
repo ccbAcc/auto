5d1b6acc8e95622e332525ed19e56a32788d41dc
ccbAcc <x-zhang@credicorpbank.com>
2026-05-20 10:13:15 -0500

    removed lines without importance in .mocharc.js

--- .mocharc.js
+++ .mocharc.js
@@ -20,18 +20,10 @@ function testRunDir() {
         const date = new Date();
         let currentDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate().toString().padStart(2, '0')}-${date.getHours()}.${date.getMinutes()}.${date.getMilliseconds()}`;
 
-        if(fs.existsSync(`./results/${currentDate}`)) {
-            console.log("Existe el directorio");
-        }
-        else {
-            fs.mkdirSync(`./results/${currentDate}`);
-            console.log("Se creó el directorio results");
-        }
-
         process.env.REPORT_DIR = `./results/${currentDate}`;
         return `./results/${currentDate}`;
     } catch(err) {
-        console.error('Error en la creación de la carpeta: ', err);
+        console.error('Error en la creación de la carpeta de reporte: ', err);
     }
 }
 
